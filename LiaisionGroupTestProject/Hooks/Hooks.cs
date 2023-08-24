f2f1045206756a6c0cc5064477aebebe42d1c7e4
Maurvi <maurvi21@gmail.com>
2023-08-24 07:50:39 +0530

    Add project files.

---
+++ LiaisionGroupTestProject/Hooks/Hooks.cs
@@ -0,0 +1,44 @@
+﻿using BoDi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.IO;
+using OpenQA.Selenium.Interactions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LiaisionGroupTestProject.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+        private readonly IObjectContainer container;
+        
+        public Hooks(IObjectContainer container)
+        {
+            this.container = container;
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario( )
+        {
+            ChromeDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            container.RegisterInstanceAs<IWebDriver>(driver);
+            //TODO: implement logic that has to run before executing each scenario
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            var driver = container.Resolve<IWebDriver>();
+            driver.Quit();
+            //TODO: implement logic that has to run after executing each scenario
+        }
+    }
+}
---
+++ LiaisionGroupTestProject/Steps/VerifyLiaisonGroupWebsiteSteps.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.IO;
+using OpenQA.Selenium.Interactions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LiaisionGroupTestProject.Steps
+{
+    [Binding]
+    public class VerifyLiaisonGroupWebsiteSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private readonly IWebDriver driver;
+        public static string URLBeforeClickFindMore = "";
+        public static string URLafterClickFindMore = "";
+        public VerifyLiaisonGroupWebsiteSteps(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        
+        [Given(@": Launch Lisison Group Website")]
+        public void GivenLaunchLisisonGroupWebsite()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            driver.Navigate().GoToUrl("https://www.liaisongroup.com/");
+            //Click on Accept Cookie button
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            if (driver.FindElement(By.XPath("//button[@data-tid='banner-accept']")).Displayed)
+            {
+                driver.FindElement(By.XPath("//button[@data-tid='banner-accept']")).Click();
+            }
+
+        }
+
+        [When(@": Open Integrated Care System Dropdown by clicking on that")]
+        public void WhenOpenIntegratedCareSystemDropdownByClickingOnThat()
+        {
+            IWebElement el = driver.FindElement(By.XPath("//div[@class='z-10 flex items-center justify-center cursor-pointer align middle']//a[contains(text(),'Integrated Care Systems')]"));
+            Actions ac = new Actions(driver);
+            ac.MoveToElement(el).Build().Perform();
+        }
+
+        [When(@": Click on Liaison Financial Logo")]
+        public void WhenClickOnLiaisonFinancialLogo()
+        {
+            URLBeforeClickFindMore = driver.Url;
+            driver.FindElement(By.XPath("//nav//div[@class='items-center hidden space-x-2 sm:hidden md:flex lg:flex lg:space-x-4']//a[@href='https://www.liaisongroup.com/liaison-financial']")).Click();
+        }
+
+
+        [When(@": Enter search value ""(.*)"" in search box")]
+        public void WhenEnterSearchValueInSearchBox(string searchvalue)
+        {
+            driver.FindElement(By.XPath("(//form[@class='relative flex items-center mb-6']//input[@placeholder='Search'])[2]")).Click();
+            driver.FindElement(By.XPath("(//form[@class='relative flex items-center mb-6']//input[@placeholder='Search'])[2]")).SendKeys(searchvalue);
+        }
+
+        [When(@": click on seatch icon")]
+        public void WhenClickOnSeatchIcon()
+        {
+            driver.FindElement(By.XPath("(//button[@type='submit'])[2]")).Click();
+        }
+
+        [When(@": Click on Hamburger menu")]
+        public void WhenClickOnHamburgerMenu()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.FindElement(By.XPath("(//button[@aria-label='Main menu'])[1]")).Click();
+        }
+
+        [When(@": Click on Careers link")]
+        public void WhenClickOnCareersLink()
+        {
+
+            driver.FindElement(By.XPath("//a[@href='/liaison-group/careers']")).Click();
+        }
+
+        [Then(@": Verify user should be able to see dropdown options")]
+        public void ThenVerifyUserShouldBeAbleToSeeDropdownOptions()
+        {
+            IWebElement el = driver.FindElement(By.XPath("//div[@class='z-10 flex items-center justify-center cursor-pointer align middle']//a[contains(text(),'Integrated Care Systems')]//parent::div//following-sibling::div"));
+            Assert.IsTrue(el.Displayed, "Dropdown element is not displayed");
+
+        }
+
+        [Then(@": Verify User should be able to redirect to another page")]
+        public void ThenVerifyUserShouldBeAbleToRedirectToAnotherPage()
+        {
+            URLafterClickFindMore = driver.Url;
+            Assert.AreNotEqual(URLafterClickFindMore, URLBeforeClickFindMore, "Find out navigation is not working");
+        }
+
+        [Then(@": Verify user should be able to see search results")]
+        public void ThenVerifyUserShouldBeAbleToSeeSearchResults()
+        {
+            string ExpectedText = "Search results for 'About Us'";
+            string ActualText = driver.FindElement(By.XPath("//div/h1[contains(text(),'About')]")).Text;
+            Assert.IsTrue(ExpectedText.Contains(ActualText), "Search is not working ");
+        }
+
+        [Then(@": verify user should be able to see carrers page with text ""(.*)""")]
+        public void ThenVerifyUserShouldBeAbleToSeeCarrersPageWithText(string p0)
+        {
+            string carrerURL = driver.Url;
+            Assert.IsTrue(carrerURL.Contains("/careers"), "Carrer page navigation is not working");
+        }
+    }
+}
